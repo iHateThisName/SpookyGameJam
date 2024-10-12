8426a19c42202b889d8014f87eb2ce6399ba14e2
ValdemarHermannss <valdi04h@gmail.com>
2024-10-12 13:29:45 +0200

    Removed Diagonal movement from the script

--- Assets/_Scripts/MovementScript.cs
+++ Assets/_Scripts/MovementScript.cs
@@ -24,18 +24,25 @@ public class MovementScript : MonoBehaviour
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
 
-        Vector2 movementDirection = new Vector2(horizontalInput, verticalInput);
+       if (Mathf.Abs(horizontalInput) > Mathf.Abs(verticalInput))
+        {
+            verticalInput = 0;
+        }
+        else
+        {
+            horizontalInput = 0;
+        }
+
+       Vector2 movementDirection = new Vector2(horizontalInput, verticalInput);
         float inputMagnitude = Mathf.Clamp01(movementDirection.magnitude);
         movementDirection.Normalize();
 
         transform.Translate(movementDirection * speed * inputMagnitude * Time.deltaTime, Space.World);
 
-
         if (movementDirection != Vector2.zero)
         {
             Quaternion toRotation = Quaternion.LookRotation(Vector3.forward, movementDirection);
             transform.rotation = Quaternion.RotateTowards(transform.rotation, toRotation, rotationSpeed * Time.deltaTime);
-
-        } 
+        }
     }
 }
