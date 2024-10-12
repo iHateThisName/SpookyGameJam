bc949b3e756be10e6387cf47585623b0f6904aec
Ben <bebru0048@student.nord.no>
2024-10-12 18:05:09 +0200

    Bin with circle ui

---
+++ Assets/_Scripts/CircleSliderManager.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CircleSliderManager : MonoBehaviour
+{
+    [Range(0f, 100f)]
+    public float fillValue = 0;
+    public Image circleFillImage;
+    public RectTransform handlerEdgeImage;
+    public RectTransform fillHandler;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        FillCircleValue(fillValue);
+    }
+
+    void FillCircleValue(float value)
+    {
+        float fillAmount = (value / 100.0f);
+        circleFillImage.fillAmount = fillAmount;
+        float angle = fillAmount * 160;
+        fillHandler.localEulerAngles = new Vector3(0, 0, -angle);
+        handlerEdgeImage.localEulerAngles = new Vector3(0, 0, angle);
+    }
+}
