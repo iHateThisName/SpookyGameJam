f6809ce1d173cc70812a7558f48fc1a26d02f6f9
Ivar Bredeli <ivabr@stud.ntnu.no>
2024-10-12 17:55:15 +0200

    Dialog sample

--- Assets/_Scripts/DialogueManager.cs
+++ Assets/_Scripts/DialogueManager.cs
@@ -2,17 +2,38 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DialogueManager : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+public class DialogueManager : MonoBehaviour {
+
+    public GameObject[] dialogText;
+    private int currentDialogueIndex = 0;
+
+    private void Start() {
+        //sort dialog text based on name
+        //System.Array.Sort(dialogText, (a, b) => string.Compare(a.name, b.name));
+
+        //foreach (GameObject dialog in dialogText) {
+        //    Debug.Log(dialog.name);
+
+        //}
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    private void Update() {
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            NextDialogue();
+        }
+    }
+
+    private void SetDialogueTextActive(int index) {
+        if (index <= dialogText.Length - 1) {
+            dialogText[index].SetActive(true);
+        } else {
+            Debug.Log("Load next Scene dialog finished");
+        }
+    }
+
+    private void NextDialogue() {
+        dialogText[currentDialogueIndex].SetActive(false);
+        currentDialogueIndex++;
+        SetDialogueTextActive(currentDialogueIndex);
     }
 }
